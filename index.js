ac042324629da2839b745f685f677751ba74247d
Dion Beau Gierman <dion.gierman@gmail.com>
2026-04-21 11:19:05 +0200

    front page

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 client.on('messageCreate', (message) => {
-  if (message.author.bot) return;
+    if (message.author.bot) return;
 
-  console.log(`${message.author.id} sent: ${message.content}`);
+    console.log(`${message.author.id} sent: ${message.content}`);
 
-  // here is where you will:
-  // - update last_active in DB
-  // - check if user was AFK
-  // - remove AFK if needed
 });
