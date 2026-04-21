24955ecedbb0adf433626a762b25eabf6ba9c77f
Dion Beau Gierman <dion.gierman@gmail.com>
2026-04-21 12:54:32 +0200

    sql stuff added

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.DirectMessages,
+    ],
+});
+
+client.once('ready', () => {
+    console.log(`✓ Bot logged in as ${client.user.tag}`);
+});
+
 client.on('messageCreate', (message) => {
     if (message.author.bot) return;
 
-    console.log(`${message.author.id} sent: ${message.content}`);
-
+    console.log(`[${message.guild?.name || 'DM'}] ${message.author.username}: ${message.content}`);
 });
+
+client.login(process.env.TOKEN);
